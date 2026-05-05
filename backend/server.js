fcec47639665121c3a9cde840f22d7b748c0197d
Pilar Luna <pilarluna084@gmail.com>
2026-05-05 00:27:19 +0000

    feat: crear endpoint GET y almacenamiento de tareas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 
 app.use(express.json());
 
+let tasks = [];
+
+app.get('/api/tasks', (req, res) => {
+    res.json(tasks);
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor de ForIT corriendo en http://localhost:${PORT}`);
 });
